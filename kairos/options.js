13c4e21f514c87996c32d120afa1c093724dcbc7
5ql <76627407+5ql@users.noreply.github.com>
2023-05-15 00:39:45 +0100

    Update options.js

--- kairos/options.js
+++ kairos/options.js
@@ -1,11 +1,10 @@
-// Retrieve the block list from storage and display it in the text area
 chrome.storage.sync.get('blockList', function(data) {
   if (data.blockList) {
     document.querySelector('#block-list').value = data.blockList.join('\n');
   }
 });
 
-// Save the block list to storage when the save button is clicked
+
 document.querySelector('#save-btn').addEventListener('click', function() {
   var blockList = document.querySelector('#block-list').value.split('\n');
   blockList = blockList.map(function(url) {
